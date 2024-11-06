4d397daad81a395b7e64a4f90b39370c1bc6e8d5
Katarina Prlenda <kateprlenda@gmail.com>
2024-11-06 19:22:40 +0100

    Dovršila registraciju kupca,(dodala validaciju za vec koristen email)

--- IzvorniKod/mojKvart/frontend/src/pages/Signup.jsx
+++ IzvorniKod/mojKvart/frontend/src/pages/Signup.jsx
@@ -1,21 +1,39 @@
 import "../styles/signup.css"
 import { Link } from "react-router-dom";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import {useNavigate} from "react-router-dom";
 
 export function Signup() {
-
+   const navigate = useNavigate();
    const [firstName, setFirstName] = useState('')
    const [lastName, setLastName] = useState('')
    const [homeAddress, setHomeAddress] = useState('')
    const [emailAddress, setEmailAddress] = useState('')
    const [password, setPassword] = useState('')
-   const [id, setId] = useState(1)
+   const [emailExists, setEmailExists] = useState(false); // Dodato stanje za proveru emaila
 
+   useEffect(() => {
+      if (emailAddress.length > 0) {
+        fetch(`/api/kupacs`)
+          .then((response) => response.json())
+          .then((registriraniKorisnici) => {
+            const duplikat = registriraniKorisnici.some(
+              (korisnik) => korisnik.kupacEmail === emailAddress
+            );
+            setEmailExists(duplikat);
+          });
+      }
+    }, [emailAddress]);
 
    function saveNoviClan(e){
       
       e.preventDefault();
+
+      if (emailExists) {
+         //alert("Email adresa je već korištena!");
+         return;
+       }
+
       const data = {
         kupacEmail: emailAddress,
         kupacIme: firstName,
@@ -35,12 +53,26 @@ export function Signup() {
       return fetch('/api/kupacs', options)
         .then(response => {
           if (response.ok) {
-            //navigate('/students');
+            navigate('/');
+            alert("Uspješna registracija! Sada se možeš prijaviti")
             console.log("sve okej dodan")
           }
         });
   
      
+   }
+
+   function isValid() {
+      //vec koristena adresa
+      
+
+
+      return firstName.length > 0 && lastName.length > 0 && emailAddress.length > 0
+      && password.length > 0 && homeAddress.length > 0 && !emailExists ;
+    }
+  
+   function handlerEmailAdress(e){
+      setEmailAddress(e.target.value)
    }
 
    
@@ -59,12 +91,18 @@ export function Signup() {
                   <input type="text" placeholder="Home address" className="signup-inputs"  name="homeAddress" value={homeAddress}
                      onChange={(e) => setHomeAddress(e.target.value)}/>
                   <input type="email" placeholder="Email address" className="signup-inputs"  name="emailAddress" value={emailAddress}
-                     onChange={(e) => setEmailAddress(e.target.value)}/>
+                     onChange={handlerEmailAdress}/>
+                      
                   <input type="password" placeholder="Password" className="signup-inputs"  name="password" value={password}
                      onChange={(e) => setPassword(e.target.value)}/>
+                     {emailExists && (
+                        <p style={{ color: "red", marginTop: "4px" ,  marginBottom: "4px", fontWeight: "bold" }}>
+                           Email adresa je već korištena!
+                        </p>
+                        )}
                   <button type="submit" className="signup-buttons" >Sign up</button>
                   <Link to="/login">
-                     <button type="submit" className="signup-buttons" >Back to Sign in</button>
+                     <button type="submit" disabled={!isValid()}className="signup-buttons" >Back to Sign in</button>
                   </Link>
                </form>
         </div>
