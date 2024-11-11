6d168043487d9a6a897729402c3b952ac6496c42
Andrej Lukas <al54639@fer.hr>
2024-11-11 17:12:26 +0100

    Dodane i stilizirane slike za login/signup, dodane stranice i rute za /home/ popravljena responzivnost logina

---
+++ IzvorniKod/mojKvart/frontend/src/pages/PopisTrgovina.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from "../components/Navbar";
+
+
+export function PopisTrgovina() {
+   
+   return (
+      <div>
+         <Navbar/>
+         <h1>PopisTrgovina</h1>
+      </div>
+   );
+}
