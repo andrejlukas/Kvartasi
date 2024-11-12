7f42aae98396c55602a4d55e3e71b6cc95c188d2
Barbara Bobeta <bb55320@fer.hr>
2024-11-12 21:53:27 +0100

    Popravljena responzivnost navbara i home stranice

--- IzvorniKod/mojKvart/frontend/src/pages/Home.jsx
+++ IzvorniKod/mojKvart/frontend/src/pages/Home.jsx
@@ -32,43 +32,36 @@ export function Home() {
   }, []);
 
   return (
-    <div id="vani">
-      <Navbar />
-      <div className="content-wrapper">
-        <div className="container mt-4">
-          {error ? (
-            <p className="text-danger">{error}</p>
-          ) : (
-            <div id="products" className="product-section">
-              <div className="row">
-                {products.length > 0 ? (
-                  products.map((product) => (
-                    <div key={product.proizvodId} className="col-md-4 mb-3">
-                      <div className="card product-card">
+    <div id="vani2">
+  <Navbar />
+  <div id="products-alt" className="product-section-alt">
+    <div className="row">
+        {products.length > 0 ? (
+            products.map((product) => (
+                <div id="productsbox" key={product.proizvodId} className="col-lg-4 col-md-6 col-12 mb-3-alt">
+                    <div className="card-alt product-card-alt">
                         <img 
                           src={product.proizvodSlika} 
-                          className="card-img-top" 
+                          className="card-img-top-alt" 
                           alt={product.proizvod_naziv} 
                         />
-                        <div className="card-body">
-                          <h5 className="card-title">{product.proizvodNaziv}</h5>
-                          <p className="card-text">{product.proizvodOpis}</p>
-                          <div id="gumbcijena">
-                            <p className="price">€{product.proizvodCijena}</p>
-                            <button className="add-to-cart-btn">Dodaj u košaricu</button>
-                          </div>
+                        <div className="card-body-alt">
+                            <h5 className="card-title-alt">{product.proizvodNaziv}</h5>
+                            <p className="card-text-alt">{product.proizvodOpis}</p>
+                            <div id="gumbcijena-alt">
+                                <p className="price-alt">€{product.proizvodCijena}</p>
+                                <button className="add-to-cart-btn-alt">Dodaj u košaricu</button>
+                            </div>
                         </div>
-                      </div>
                     </div>
-                  ))
-                ) : (
-                  <p>Nisu dostupni proizvodi.</p>
-                )}
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
+                </div>
+            ))
+        ) : (
+            <p>Nisu dostupni proizvodi.</p>
+        )}
     </div>
+</div>
+</div>
+
   );
 }
