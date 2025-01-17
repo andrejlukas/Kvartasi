72cb426d5182e423be94d79d1f5ec2e540c2b7a6
Andrej Lukas <andrej.lukas1a@gmail.com>
2025-01-17 12:47:13 +0100

    radi odobravanje i odbijanje proizvoda za moderatora

--- IzvorniKod/mojKvart/frontend/src/pages/moderator/Home.jsx
+++ IzvorniKod/mojKvart/frontend/src/pages/moderator/Home.jsx
@@ -1,17 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Navbar } from '../../components/ModeratorNavbar'
+import { Navbar } from '../../components/ModeratorNavbar';
 import "../../styles/Home.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export function ModeratorHome(){
-
+export function ModeratorHome() {
     const [productsType, setProductsType] = useState("approved");
     const [error, setError] = useState("");
     const [approvedProducts, setApprovedProducts] = useState([]);
     const [notApprovedProducts, setNotApprovedProducts] = useState([]);
-
     const [products, setProducts] = useState([]);
-  
 
     useEffect(() => {
         const token = localStorage.getItem('token');
@@ -21,7 +18,7 @@ export function ModeratorHome(){
                 'Authorization': `Bearer ${token}`,
                 'Content-Type': 'application/json'
             }
-        }
+        };
 
         fetch(`/api/proizvods/approved`, options)
             .then(async (response) => {
@@ -33,7 +30,9 @@ export function ModeratorHome(){
             })
             .then((data) => {
                 setApprovedProducts(data);
-                setProducts(data);
+                if (productsType === "approved") {
+                    setProducts(data);
+                }
             })
             .catch((error) => {
                 setError(error.message);
@@ -49,50 +48,96 @@ export function ModeratorHome(){
             })
             .then((data) => {
                 setNotApprovedProducts(data);
-                console.log("aloo");
+                if (productsType === "notApproved") {
+                    setProducts(data);
+                }
             })
             .catch((error) => {
                 setError(error.message);
             });
-
-    }, []);
+    }, [productsType]);
 
     const handleProductsTypeChange = (e) => {
         setProductsType(e.target.value);
-        if(e.target.value === "approved") setProducts(approvedProducts);
-        else setProducts(notApprovedProducts);
+        if (e.target.value === "approved") {
+            setProducts(approvedProducts);
+        } else {
+            setProducts(notApprovedProducts);
+        }
     };
 
-    const OdobriProizvod = (proizvodId) => {
-        console.log("odobri "+proizvodId)
+    function updateProduct(productData) {
+        const token = localStorage.getItem('token');
+        const options = {
+            method: 'PUT',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(productData)
+        };
+
+        fetch(`/api/proizvods/${productData.proizvodId}`, options)
+            .then(async (response) => {
+                if (!response.ok) {
+                    const text = await response.text();
+                    throw new Error(text);
+                }
+            })
+            .then(() => {
+                setNotApprovedProducts((prevProducts) =>
+                    prevProducts.filter((product) => product.proizvodId !== productData.proizvodId)
+                );
+                if (productData.proizvodFlag === "A") {
+                    setApprovedProducts((prevProducts) => [...prevProducts, productData]);
+                }
+                if (productsType === "approved") {
+                    setProducts((prevProducts) => [...prevProducts, productData]);
+                } else {
+                    setProducts((prevProducts) =>
+                        prevProducts.filter((product) => product.proizvodId !== productData.proizvodId)
+                    );
+                }
+            })
+            .catch((error) => {
+                setError(error.message);
+            });
     }
-    const OdbijProizvod = (proizvodId) => {
-        console.log("nije odobren "+proizvodId)
-    }
+
+    const OdobriProizvod = (product) => {
+        product.proizvodFlag = "A";
+        updateProduct(product);
+    };
+
+    const OdbijProizvod = (product) => {
+        product.proizvodFlag = "R";
+        updateProduct(product);
+    };
+
     return (
         <div>
-            <div id="vani2" style={{"minHeight": "100vh"}}>
+            <div id="vani2" style={{ minHeight: "100vh" }}>
                 <Navbar />
-                {/* {error && <p>{error}</p>} */}
+                {error && <p>{error}</p>}
                 <div id="products-alt" className="product-section-alt">
                     <div id="productController">
                         <label>
                             <input
-                            type="radio"
-                            name="productsType"
-                            value="approved"
-                            checked={productsType === "approved"}
-                            onChange={handleProductsTypeChange}
+                                type="radio"
+                                name="productsType"
+                                value="approved"
+                                checked={productsType === "approved"}
+                                onChange={handleProductsTypeChange}
                             />
                             Odobreni proizvodi
                         </label>
                         <label>
                             <input
-                            type="radio"
-                            name="productsType"
-                            value="notApproved"
-                            checked={productsType === "notApproved"}
-                            onChange={handleProductsTypeChange}
+                                type="radio"
+                                name="productsType"
+                                value="notApproved"
+                                checked={productsType === "notApproved"}
+                                onChange={handleProductsTypeChange}
                             />
                             Proizvodi koji čekaju odobrenje
                         </label>
@@ -103,9 +148,9 @@ export function ModeratorHome(){
                                 <div id="productsbox" key={product.proizvodId} className="col-lg-4 col-md-6 col-12 mb-3-alt">
                                     <div className="card-alt product-card-alt">
                                         <img 
-                                        src={product.proizvodSlika} 
-                                        className="card-img-top-alt" 
-                                        alt={product.proizvod_naziv} 
+                                            src={product.proizvodSlika} 
+                                            className="card-img-top-alt" 
+                                            alt={product.proizvod_naziv} 
                                         />
                                         <div className="card-body-alt">
                                             <h5 className="card-title-alt">{product.proizvodNaziv}</h5>
@@ -114,8 +159,8 @@ export function ModeratorHome(){
                                                 <p className="price-alt">€{product.proizvodCijena}</p>
                                                 {productsType === "notApproved" && (
                                                     <>
-                                                        <button className="add-to-cart-btn-alt" onClick={() => OdobriProizvod(product.proizvodId)}>Odobri proizvod</button>
-                                                        <button className="add-to-cart-btn-alt" onClick={() => OdbijProizvod(product.proizvodId)}>Odbij proizvod</button>
+                                                        <button className="add-to-cart-btn-alt" onClick={() => OdobriProizvod(product)}>Odobri proizvod</button>
+                                                        <button className="add-to-cart-btn-alt" onClick={() => OdbijProizvod(product)}>Odbij proizvod</button>
                                                     </>
                                                 )}
                                             </div>
@@ -129,7 +174,6 @@ export function ModeratorHome(){
                     </div>
                 </div>
             </div>
-            
         </div>
     );
 }
