529548c36aee6d966402177e2bbc075d408e4dcc
jhajpek <josip.hajpek@fer.hr>
2025-01-18 01:38:00 +0100

    jos jedan pokusaj

--- IzvorniKod/mojKvart/frontend/vite.config.js
+++ IzvorniKod/mojKvart/frontend/vite.config.js
@@ -2,25 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { loadEnv } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
     base: "/",
     plugins: [react()],
-    preview: {
+    server: {
       port: 3000,
       strictPort: true,
-    },
-    server: {
-    port: 3000,
-    strictPort: true,
-    host: true,
+      host: true,
       proxy: {
         '/api': {
           target: "https://kvartasialfabackend.onrender.com",
-          secure: true,
+          changeOrigin: true,
+          secure: true
         },
       },
     },
